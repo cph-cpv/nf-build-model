93eab3973320c5f2234bec69129f2841b6cfe322
Reece Hoffmann <59776400+ReeceHoffmann@users.noreply.github.com>
2025-04-25 12:14:08 -0700

    feat: create unreliable regions based on nucleotide composition

--- scripts/extract_sequence_info.py
+++ scripts/extract_sequence_info.py
@@ -2,22 +2,19 @@ import csv
 import json
 from collections import Counter, defaultdict
 import sys
+from Bio import SeqIO
 
-reference_path = sys.argv[1]
-
-with open(reference_path) as f:
-    reference_json = json.load(f)
+reference_fasta_path = sys.argv[1]
+output_path = sys.argv[2]
 
 
-with open("scripts/output/nucleotide_info.csv", "w") as f:
+with open(reference_fasta_path) as reference_fasta, open(output_path, "w") as f:
+    reference = SeqIO.parse(reference_fasta, "fasta")
+
     nucleotide_info = csv.writer(f)
     nucleotide_info.writerow(
         [
             "sequence_id",
-            "virus_acronym",
-            "virus_id",
-            "virus_name",
-            "isolate_id",
             "a_percent",
             "c_percent",
             "t_percent",
@@ -26,25 +23,16 @@ with open("scripts/output/nucleotide_info.csv", "w") as f:
         ]
     )
     
-    for otu in reference_json["otus"]:
+    for record in reference:
+        counter = Counter(record.seq.lower())
 
-        for isolate in otu["isolates"]:
-            isolate_id = isolate["id"]
-
-            for sequence in isolate["sequences"]:
-                counter = Counter(sequence["sequence"].lower())
-
-                nucleotide_info.writerow(
-                    [   
-                        sequence["_id"],
-                        otu["abbreviation"]
-                        otu["_id"],
-                        otu["name"].lower(),
-                        isolate_id,
-                        counter["a"] / counter.total(),
-                        counter["c"] / counter.total(),
-                        counter["t"] / counter.total(),
-                        (counter["g"] + counter["c"]) / counter.total(),
-                        len(sequence["sequence"]),
-                    ]
-                )
+        nucleotide_info.writerow(
+            [   
+                record.id,
+                counter["a"] / counter.total(),
+                counter["c"] / counter.total(),
+                counter["t"] / counter.total(),
+                (counter["g"] + counter["c"]) / counter.total(),
+                len(record.seq),
+            ]
+        )
